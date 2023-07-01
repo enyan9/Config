d8e1a4da383bbcc223c2a7fd30069235df71b962
esnyang <noreply@github.com>
2023-07-01 11:44:05 -0400

    fix firefox and kde scripts

---
+++ firefox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the input as the delimiter
+IFS=" "
+
+# Prompt the user for input
+read -p "Enter words (separated by spaces): " input
+
+# Read each name into an array
+read -ra names <<< "$input"
+
+mkdir -p ~/app/firefox \
+    && wget -O ~/app/firefox/firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64" \
+    
+
+for name in "${names[@]}"; do
+    firefox --CreateProfile "$name" "$HOME/.mozilla/firefox/emet.${name}"
+    #rsync -av --progress app/firefox/.mozilla/firefox/ "${HOME}/.mozilla/firefox/"
+    mkdir -p ~/app/firefox/firefox-"$name" \
+        && tar xjf ~/app/firefox/firefox.tar.bz2 -C ~/app/firefox/firefox-"$name" --strip-components=1
+done
--- kde.sh
+++ kde.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
 kwriteconfig5 --file $HOME/.config/powermanagementprofilesrc --group AC --group DPMSControl --key idleTime --delete
-kwriteconfig5 --file $HOME/.config/powermanagementprofilesrc --group AC --group SuspendSession --key idleTime --key suspendType --key suspendThenHibernate --delete
+kwriteconfig5 --file $HOME/.config/powermanagementprofilesrc --group AC --group DimDisplay --key idleTime --delete
+kwriteconfig5 --file $HOME/.config/powermanagementprofilesrc --group AC --group SuspendSession  --key suspendType --delete
+kwriteconfig5 --file $HOME/.config/powermanagementprofilesrc --group AC --group SuspendSession --key idleTime --delete
+kwriteconfig5 --file $HOME/.config/powermanagementprofilesrc --group AC --group SuspendSession  --key suspendThenHibernate --delete
+
 qdbus org.kde.Solid.PowerManagement /org/kde/Solid/PowerManagement org.kde.Solid.PowerManagement.refreshStatus
 
 systemsettings5 kcm_screenlocker
