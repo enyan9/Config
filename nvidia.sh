196c3b85694dc176e5f0a31523bfbc7e650b305b
esnyang <noreply@github.com>
2023-07-09 22:50:34 -0400

    periodic savepoint

---
+++ nvidia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## https://rpmfusion.org/Howto/NVIDIA
+sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+## CUDA libs for ffmpeg: xorg-x11-drv-nvidia-cuda-libs
+
+modinfo -F version nvidia
+
+## https://rpmfusion.org/Howto/NVIDIA#Suspend
+
+# sudo dnf install xorg-x11-drv-nvidia-power
+# sudo systemctl enable nvidia-{suspend,resume,hibernate}
