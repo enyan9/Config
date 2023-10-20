96f46bb1b3c26ac738ce0c911e60940596e61027
enyan9 <noreply@github.com>
2023-10-20 10:23:19 -0400

    periodic savepoint

--- app/vscode/update_code_windows.sh
+++ app/vscode/update_code_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#From WSL ./update_code_windows.sh /mnt/c/Users/$USER/app 
+#From WSL ./update_code_windows.sh /mnt/c/Users/$USER/app
 
 #https://code.visualstudio.com/docs/editor/portable
 ##https://code.visualstudio.com/docs/editor/command-line
@@ -7,20 +7,22 @@
 #https://code.visualstudio.com/sha/download?build=stable&os=linux-x64
 #https://update.code.visualstudio.com/latest/linux-x64/stable
 
-ROOT_DIR=${1:-"/mnt/c/Users/""$USER"}
+ROOT_DIR=${1:-"/mnt/c/Users/""$USER"/app/vscode}
 VERSION=${2:-"latest"}
 
 ZIP="VSCode-win32-x64-${VERSION}.zip"
 ZIPX="VSCode-win32-x64-${VERSION}"
 
+mkdir -p "${ROOT_DIR}"
+
 wget "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive" -O "${ROOT_DIR}/${ZIP}"
 unzip "${ROOT_DIR}/${ZIP}" -d "${ROOT_DIR}/${ZIPX}"
 
-for app in 'base' 'notebook' 'paint' 'pdf' 'repo' 'terminal', 'aux'; do
+for app in 'base' 'notebook' 'paint' 'pdf' 'repo' 'terminal' 'aux' 'primum' 'secondary' 'tertiary' 'quartum' 'quintum' 'sextum'; do
     echo "${ROOT_DIR}/vscode-${app}/"
 
+	
     rsync -av "${ROOT_DIR}/${ZIPX}/" "${ROOT_DIR}/vscode-${app}/"
 
-    mkdir -p "${ROOT_DIR}vscode-${app}/data"
     sed -i 's/"win32AppUserModelId": "Microsoft.VisualStudioCode"/"win32AppUserModelId": "vscode-'"$app"'"/' "$ROOT_DIR""/vscode-""$app""/resources/app/product.json"
 done
