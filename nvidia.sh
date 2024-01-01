b91c2638d9439919b09b4420787c27edd57b5a86
enyan9 <noreply@github.com>
2024-01-01 18:09:07 -0500

    nvidia podman config

--- nvidia.sh
+++ nvidia.sh
@@ -11,3 +11,14 @@ sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 
 # sudo dnf install xorg-x11-drv-nvidia-power
 # sudo systemctl enable nvidia-{suspend,resume,hibernate}
+
+
+## https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installing-with-yum-or-dnf
+## https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/cdi-support.html
+
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
+  sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo && sudo dnf install -y nvidia-container-toolkit
+
+sudo nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
+
+nvidia-ctk cdi list
