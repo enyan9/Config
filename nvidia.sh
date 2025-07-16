76063f9581da6ef9c76de3dfb9848e13d6cc9a9c
enyan9 <noreply@github.com>
2025-07-15 20:14:13 -0400

    attempt

--- nvidia.sh
+++ nvidia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## https://rpmfusion.org/Howto/NVIDIA
-sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+sudo dnf --enablerepo=rpmfusion-nonfree --enablerepo=rpmfusion-free install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 ## CUDA libs for ffmpeg: xorg-x11-drv-nvidia-cuda-libs
 
 # nvidia-smi
@@ -11,6 +11,9 @@ sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 
 # sudo dnf install xorg-x11-drv-nvidia-power
 # sudo systemctl enable nvidia-{suspend,resume,hibernate}
+
+# Troubleshooting:
+# https://discussion.fedoraproject.org/t/nvidia-driver/64653/9
 
 
 ## https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installing-with-yum-or-dnf
